ca82a789a1fe728a5915e67b2e7fe22d5f8438fa
Raga Subekti <rfilmriot@gmail.com>
2019-04-02 11:33:10 +0700

    Add Provider to App

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import Routes from "./router";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import "./styles/index.css";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
--- src/views/AddTodos.js
+++ src/views/AddTodos.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import { connect } from "react-redux";
+import { addTodos } from "../modules/todos";
+
 class AddTodos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  updateInput = input => {
+    this.setState({ input });
+  };
+
+  onFormSubmit(e) {
+    e.preventDefault();
+
+    this.props.addTodos(this.state.input);
+
+    this.setState({
+      input: ""
+    });
+
+    this.props.history.push("/");
+  }
+
   render() {
     return (
       <div className="container">
@@ -12,7 +39,7 @@ class AddTodos extends Component {
           </Link>
         </section>
 
-        <form action="#" className="todos-form">
+        <form onSubmit={e => this.onFormSubmit(e)} className="todos-form">
           <fieldset className="margin-bottom-25">
             <label htmlFor="todos-content">Content</label>
             <textarea
@@ -22,16 +49,25 @@ class AddTodos extends Component {
               rows="10"
               cols="60"
               placeholder="Type your todos here..."
+              onChange={e => this.updateInput(e.target.value)}
+              value={this.state.input}
             />
           </fieldset>
 
-          <Link className="button button-add margin-to-25" to="/add">
+          <button
+            className="button button-add margin-to-25"
+            to="/add"
+            onClick={e => this.onFormSubmit(e)}
+          >
             + Add
-          </Link>
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default AddTodos;
+export default connect(
+  null,
+  { addTodos }
+)(AddTodos);
